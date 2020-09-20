73be549058ba9e70152f932b102fb5d169eb25e8
postovalovICQ <71002789+postovalovICQ@users.noreply.github.com>
2020-09-20 15:59:40 +0500

    Add files via upload

---
+++ oop1.2/oop1.2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace oop1._2
+{
+    class Program
+    {
+        class Member
+        {
+            internal int Id;
+            public string Surname;
+            public string Name;
+            public string Patronymic;
+            internal string Address;
+            internal string Creditcard;
+            internal string Time;
+        }
+        static void Main(string[] args)
+        {
+            Member Member = new Member();
+            Console.WriteLine("Введите данные абонента");
+
+            Console.Write("Введите идентификационный номер: ");
+            Member.Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите фамилию: ");
+            Member.Name = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Введите имя: ");
+            Member.Surname = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Введите отчество: ");
+            Member.Patronymic = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Введите адресс: ");
+            Member.Address = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Номер кредитной карточки: ");
+            Member.Creditcard = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Введите время междугородных и городских переговоров: ");
+            Member.Time = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("------------------------");
+            Console.WriteLine("------------------------");
+            Console.WriteLine(Member.Id);
+            Console.WriteLine(Member.Surname);
+            Console.WriteLine(Member.Name);
+            Console.WriteLine(Member.Patronymic);
+            Console.WriteLine(Member.Address);
+            Console.WriteLine(Member.Creditcard);
+            Console.WriteLine(Member.Time);
+            Console.WriteLine("------------------------");
+            Console.ReadLine();
+
+        }
+    }
+}
